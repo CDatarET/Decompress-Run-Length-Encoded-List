de3941e21c1f4d0256b288f4d5ebeccefecb707e
Chaitanya Datar <datar.chaitanya@gmail.com>
2026-01-01 23:30:23 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] decompressRLElist(int[] nums){
+        int size = 0;
+        for(int i = 0; i < nums.length; i += 2) size += nums[i];
+
+        int[] ret = new int[size];
+        int index = 0;
+        for(int i = 0; i < nums.length; i += 2) for(int j = 0; j < nums[i]; j++) ret[index++] = nums[i + 1];
+
+        return(ret);
+    }
+}
